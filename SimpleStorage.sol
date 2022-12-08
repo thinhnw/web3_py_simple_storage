fad211b1dcf15b3da13772b0fb8d94ce79076ba9
thinhnw <thinhnv0412@gmail.com>
2022-12-09 00:14:07 +0700

    Init commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+}
